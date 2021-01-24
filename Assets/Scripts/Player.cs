9ec5a07db0deacfe7863654d098b759cd02a713e
Yuya Sumie <monamonacan0926@gmail.com>
2021-01-25 00:22:32 +0900

    Player が動くようになった

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+  public float speed;
   // Start is called before the first frame update
   void Start()
   {
@@ -13,6 +14,6 @@ public class Player : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    transform.Translate(0, 0, speed * Time.deltaTime);
   }
 }
