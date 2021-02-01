45cdc5876ea9d07e0b25e58edb3a2496a6e431b7
Yuya Sumie <monamonacan0926@gmail.com>
2021-02-01 13:13:15 +0900

    スピードメーターを作った

---
+++ Assets/Scripts/OutputSpeed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutputSpeed : MonoBehaviour
+{
+  public Text speedText = null;
+  private Vector3 latestPos;
+  private Vector3 speed;
+  private GameObject player;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    player = GameObject.Find("Player");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    speed = ((player.transform.position - latestPos) / Time.deltaTime);
+    latestPos = player.transform.position;
+    speedText.text = speed.magnitude.ToString("00") + "km/h";
+  }
+}
